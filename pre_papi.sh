50c22f5c815b8906f546c52fa023d2831eddf09e
Michal Kaminski <michkam89@gmail.com>
2019-01-05 18:57:40 +0100

    pre_papi done

--- pre_papi.sh
+++ pre_papi.sh
@@ -13,19 +13,37 @@ cat << _EOF_
 
 Licensed under GNU General Public License v2.0
 
+THIS IS PREPROCESSING SCRIPT FOR MAIN PAPi ANALYSES
 _EOF_
+
+# PARAMETERS
 
 # If test data run
 MODE=$1
+# File with names of genomes used in the analysis
+GNAMES=gnames_file.csv
 
+
+# Run test or user mode
 if [ "$MODE" = "test" ]; then
-  echo "########## PAPi RUNNING IN TEST MODE"
+  echo "########## pre-PAPi RUNNING IN TEST MODE"
   INPUT=./data/sph_faa
   OUTPUT=./output
+  PERM=100
 else
-  echo "########## PAPi ANALYSIS RUNNING WITH USER INPUT"
+  echo "########## pre-PAPi ANALYSIS RUNNING WITH USER INPUT"
   INPUT=$2
   OUTPUT=$3
+  #PERM=$4
+fi
+
+# output for core clusters results
+#CORECLUSTO=$OUTPUT/core_clust_out
+
+# Assertions
+if [[ ! -d "$INPUT" ]]; then
+  echo "INPUT DIRECTORY $INPUT DOES NOT EXIST"
+  exit 1
 fi
 
 # Run the preformating of protein fasta FILES
@@ -34,3 +52,13 @@ python3 ./code/Prep_Init_Faa_Files.py --input $INPUT --output $OUTPUT"
 
 echo "########## Formatting fasta files..."
 python3 ./code/Prep_Init_Faa_Files.py --input $INPUT --output $OUTPUT
+
+# check whether gnames_file was created
+if [[ -e "$GNAMES" ]]; then
+  echo "########## gnames_file.csv created"
+else
+  echo "########## gnames_file not created, exiting"
+  stop
+fi
+
+echo "NOW PLEASE RUN CD-HIT FOR PROTEIN CLUSTERING"
